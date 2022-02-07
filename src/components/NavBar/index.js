455d04a1667811d0bc332acbd5bd5b6676c843bb
victor <bodegaflamenca666@gmail.com>
2022-02-07 17:16:30 +0100

    smartphone Menu

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -2,6 +2,14 @@ import {useState, useContext} from "react";
 import {CSSTransition} from "react-transition-group";
 import styled from "styled-components";
 import TelosLogo from "../SVG/telos_letter_logo.svg";
+import Locally from "../SVG/cryptolocally_logo.svg";
+import Kucoin from "../SVG/kucoin_logo.svg";
+import Gate from "../images/gate_logo.png";
+import Pancake from "../images/pancake_logo.png";
+import Uniswap from "../images/uniswap_logo.png";
+import ESG from "../images/esg_logo.png";
+import EVM from "../images/evm_logo.png";
+import TelosRoundLogo from "../images/telos_logo.png";
 import { motion } from "framer-motion";
 import {IconButton} from "@material-ui/core";
 import StorefrontIcon from '@mui/icons-material/Storefront';
@@ -11,6 +19,11 @@ import ExploreIcon from '@mui/icons-material/Explore';
 import HelpCenterIcon from '@mui/icons-material/HelpCenter';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import DeveloperBoardIcon from '@mui/icons-material/DeveloperBoard';
+import LocalFireDepartmentIcon from '@mui/icons-material/LocalFireDepartment';
+import SourceIcon from '@mui/icons-material/Source';
+import ArticleIcon from '@mui/icons-material/Article';
+import MapIcon from '@mui/icons-material/Map';
 import { Button } from "@mui/material";
 import NewspaperIcon from '@mui/icons-material/Newspaper';
 import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
@@ -21,7 +34,225 @@ import MenuIcon from '@mui/icons-material/Menu';
 import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 import LightModeIcon from '@mui/icons-material/LightMode';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
+import ContactSupportIcon from '@mui/icons-material/ContactSupport';
+import GroupWorkIcon from '@mui/icons-material/GroupWork';
+import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
+import ManageSearchIcon from '@mui/icons-material/ManageSearch';
+import AirlineStopsIcon from '@mui/icons-material/AirlineStops';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import YardIcon from '@mui/icons-material/Yard';
+import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
 import "./style.css";
+
+const MeetBoardIcon = styled(HailIcon)`
+    &&&{
+        color: #ba55d3;
+        border-radius: 50%;
+        cursor: pointer;
+        transition: filter 300ms;
+        &:hover{
+            transform: scale(1.2);
+            filter: brightness(1.2);
+        }
+    }
+`;
+
+const MeetTeamIcon = styled(EmojiPeopleIcon)`
+    &&&{
+        color: #ba55d3;
+        border-radius: 50%;
+        cursor: pointer;
+        transition: filter 300ms;
+        &:hover{
+            transform: scale(1.2);
+            filter: brightness(1.2);
+        }
+    }
+`;
+
+const JoinIcon = styled(GroupAddIcon)`
+    &&&{
+        color: #ba55d3;
+        border-radius: 50%;
+        cursor: pointer;
+        transition: filter 300ms;
+        &:hover{
+            transform: scale(1.2);
+            filter: brightness(1.2);
+        }
+    }
+`;
+
+
+
+const NewsIcon = styled(NewspaperIcon)`
+    &&&{
+        color: #ba55d3;
+        border-radius: 50%;
+        cursor: pointer;
+        transition: filter 300ms;
+        &:hover{
+            transform: scale(1.2);
+            filter: brightness(1.2);
+        }
+    }
+`;
+
+const Wallet = styled(AccountBalanceWalletIcon)`
+    &&&{
+        color: #ba55d3;
+        border-radius: 50%;
+        cursor: pointer;
+        transition: filter 300ms;
+        &:hover{
+            transform: scale(1.2);
+            filter: brightness(1.2);
+        }
+    }
+`;
+
+const EcoIcon = styled(YardIcon)`
+    &&&{
+        color: #ba55d3;
+        border-radius: 50%;
+        cursor: pointer;
+        transition: filter 300ms;
+        &:hover{
+            transform: scale(1.2);
+            filter: brightness(1.2);
+        }
+    }
+`;
+
+const GetStarted = styled(PlayArrowIcon)`
+    &&&{
+        color: #ba55d3;
+        border-radius: 50%;
+        cursor: pointer;
+        transition: filter 300ms;
+        &:hover{
+            transform: scale(1.2);
+            filter: brightness(1.2);
+        }
+    }
+`;
+
+const EVMBridge = styled(AirlineStopsIcon)`
+    &&&{
+        color: #ba55d3;
+        border-radius: 50%;
+        cursor: pointer;
+        transition: filter 300ms;
+        &:hover{
+            transform: scale(1.2);
+            filter: brightness(1.2);
+        }
+    }
+`;
+
+const EVMExplorer = styled(ManageSearchIcon)`
+    &&&{
+        color: #ba55d3;
+        border-radius: 50%;
+        cursor: pointer;
+        transition: filter 300ms;
+        &:hover{
+            transform: scale(1.2);
+            filter: brightness(1.2);
+        }
+    }
+`;
+
+const TokenIcon = styled(MonetizationOnIcon)`
+    &&&{
+        color: #ba55d3;
+        border-radius: 50%;
+        cursor: pointer;
+        transition: filter 300ms;
+        &:hover{
+            transform: scale(1.2);
+            filter: brightness(1.2);
+        }
+    }
+`;
+
+const GovIcon = styled(GroupWorkIcon)`
+    &&&{
+        color: #ba55d3;
+        border-radius: 50%;
+        cursor: pointer;
+        transition: filter 300ms;
+        &:hover{
+            transform: scale(1.2);
+            filter: brightness(1.2);
+        }
+    }
+`;
+
+const BaseIcon = styled(ContactSupportIcon)`
+    &&&{
+        color: #ba55d3;
+        border-radius: 50%;
+        cursor: pointer;
+        transition: filter 300ms;
+        &:hover{
+            transform: scale(1.2);
+            filter: brightness(1.2);
+        }
+    }
+`;
+
+const CommIcon = styled(SourceIcon)`
+    &&&{
+        color: #ba55d3;
+        border-radius: 50%;
+        cursor: pointer;
+        transition: filter 300ms;
+        &:hover{
+            transform: scale(1.2);
+            filter: brightness(1.2);
+        }
+    }
+`;
+
+const GrantIcon = styled(LocalFireDepartmentIcon)`
+    &&&{
+        color: #ba55d3;
+        border-radius: 50%;
+        cursor: pointer;
+        transition: filter 300ms;
+        &:hover{
+            transform: scale(1.2);
+            filter: brightness(1.2);
+        }
+    }
+`;
+
+const DocsIcon = styled(ArticleIcon)`
+    &&&{
+        color: #ba55d3;
+        border-radius: 50%;
+        cursor: pointer;
+        transition: filter 300ms;
+        &:hover{
+            transform: scale(1.2);
+            filter: brightness(1.2);
+        }
+    }
+`;
+
+const DevIcon = styled(DeveloperBoardIcon)`
+    &&&{
+        color: #ba55d3;
+        border-radius: 50%;
+        cursor: pointer;
+        transition: filter 300ms;
+        &:hover{
+            transform: scale(1.2);
+            filter: brightness(1.2);
+        }
+    }
+`;
 
 const Nav = styled.div`
     height: 80px;
@@ -48,6 +279,12 @@ const StyledButton = styled(Button)`
         font-size: 16px;
         border: 2px solid #ba55d3;
         padding: 5px 15px;
+        transition: filter 300ms;
+        cursor: pointer;
+        &:hover{
+            transform: scale(1.2);
+            filter: brightness(1.2);
+        }
         @media screen and (max-width: 1100px){
             display: none;
         }
@@ -69,6 +306,7 @@ const SwitchIcon = styled.div`
 const Burguer = styled(MenuIcon)`
     &&&{
         color: ${props => props.theme.text};
+        
         @media screen and (min-width: 1100px){
             display: none;
         }
@@ -97,17 +335,21 @@ const AboutIcon = styled(HelpCenterIcon)`
         color: #ba55d3;
         border-radius: 50%;
         cursor: pointer;
+        transition: filter 300ms;
         &:hover{
             transform: scale(1.2);
+            filter: brightness(1.2);
         }
     }
 `;
 const ExpIcon = styled(ExploreIcon)`
     &&&{
         color: #ba55d3;
+        transition: filter 300ms;
         cursor: pointer;
         &:hover{
             transform: scale(1.2);
+            filter: brightness(1.2);
         }
     }
 `;
@@ -115,9 +357,11 @@ const ExpIcon = styled(ExploreIcon)`
 const LearnIcon = styled(SchoolIcon)`
     &&&{
         color: #ba55d3;
+        transition: filter 300ms;
         cursor: pointer;
         &:hover{
             transform: scale(1.2);
+            filter: brightness(1.2);
         }
     }
 `;
@@ -126,8 +370,10 @@ const BuildIcon = styled(ConstructionIcon)`
     &&&{
         color: #ba55d3;
         cursor: pointer;
+        transition: filter 300ms;
         &:hover{
             transform: scale(1.2);
+            filter: brightness(1.2);
         }
     }
 `;
@@ -136,8 +382,10 @@ const BuyIcon = styled(StorefrontIcon)`
     &&&{
         color: #ba55d3;
         cursor: pointer;
+        transition: filter 300ms;
         &:hover{
             transform: scale(1.2);
+            filter: brightness(1.2);
         }
     }
 `;
@@ -234,6 +482,18 @@ const SubMenu = styled.div`
     border-radius: 20px;
 `;
 
+const RoadMapIcon = styled(MapIcon)`
+    &&&{
+        color: #ba55d3;
+        transition: filter 300ms;
+        cursor: pointer;
+        &:hover{
+            transform: scale(1.2);
+            filter: brightness(1.2);
+        }
+    }
+`;
+
 const Wrapper = styled.div`
     display: flex;
     flex-direction: column;
@@ -255,10 +515,17 @@ const SmartphoneMenu = styled.div`
 
 const List = styled.li`
     list-style: none;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `;
 
 const MenuIconTwo = styled.a`
-
+    padding: 5px;
+    margin: 2px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `;
 
 const NavItem = (props) => {
@@ -281,11 +548,12 @@ const MenuMenuItem = styled.a`
     border-radius: 8px;
     align-items: center;
     justify-content: right;
-    padding
     transition: background var(--speed);
     &:hover{
-        background-color: #ba55d3;
+        background-color: #dda0dd;
     }
+
+    
 `;
 const IconLeft = styled.span`
     padding-left: 20px;
@@ -296,19 +564,23 @@ const IconRight = styled.span`
     transform: scale(1.2);
     padding-right: 20px;
 `;
+const IconMiddle = styled.span`
+    margin-left: auto;
+    margin-right: auto;
+    transform: scale(1.2);
+    padding-left: 40px;
+`;
 const DropDown = styled.div`
     position: absolute;
     width: 300px;
-    height: 400px;
-    transform: translateX(-45%);
     border-radius: 20px;
     background: black;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    border: 2px solid #ba55d3;
+    overflow: hidden;
     top: 100px;
     left: 160px;
+    @media screen and (max-width: 1100px){
+        left: 30px;
+    }
 `;
 
 const Menu = styled.div`
@@ -323,7 +595,9 @@ const DropDownMenu = () => {
         return(
             <MenuMenuItem onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}>
                 <IconLeft>{props.leftIcon}</IconLeft>
+                
                 {props.children}
+                <IconMiddle>{props.middleIcon}</IconMiddle>
                 <IconRight>{props.rightIcon}</IconRight>
             </MenuMenuItem>
         );
@@ -331,30 +605,107 @@ const DropDownMenu = () => {
     
     return(
         <>
-        <CSSTransition in={activeMenu === 'main'} unmountOnExit timeout={500} classNames="menu-primary">
+        
         <DropDown>
+        <CSSTransition in={activeMenu === 'main'} unmountOnExit timeout={500} classNames="menu-primary">
             <Menu>
             <DropDownItem leftIcon={<KeyboardArrowLeftIcon/>} rightIcon={<BuyIcon />} goToMenu="buy">BUY</DropDownItem>
-            <DropDownItem leftIcon={<KeyboardArrowLeftIcon/>} rightIcon={<BuildIcon/>}>BUILD</DropDownItem>
-            <DropDownItem leftIcon={<KeyboardArrowLeftIcon/>} rightIcon={<LearnIcon/>}>LEARN</DropDownItem>
-            <DropDownItem leftIcon={<KeyboardArrowLeftIcon/>} rightIcon={<ExpIcon/>}>EXPLORE</DropDownItem>
-            <DropDownItem leftIcon={<KeyboardArrowLeftIcon/>} rightIcon={<AboutIcon/>}>ABOUT</DropDownItem>
+            <DropDownItem leftIcon={<KeyboardArrowLeftIcon/>} rightIcon={<BuildIcon/>}goToMenu="build">BUILD</DropDownItem>
+            <DropDownItem leftIcon={<KeyboardArrowLeftIcon/>} rightIcon={<LearnIcon/>} goToMenu='learn'>LEARN</DropDownItem>
+            <DropDownItem leftIcon={<KeyboardArrowLeftIcon/>} rightIcon={<ExpIcon/>} goToMenu='evm-native'>EXPLORE</DropDownItem>
+            <DropDownItem leftIcon={<KeyboardArrowLeftIcon/>} rightIcon={<AboutIcon/>} goToMenu="about">ABOUT</DropDownItem>
             </Menu>
+            </CSSTransition>
         </DropDown>
-        </CSSTransition>
+        
 
-        <CSSTransition in={activeMenu === 'buy'} unmountOnExit timeout={500} classNames="menu-secondary">
+        
         <DropDown>
+        <CSSTransition in={activeMenu === 'buy'} unmountOnExit timeout={500} classNames="menu-secondary">
             <Menu>
-            <DropDownItem rightIcon={<KeyboardArrowRightIcon/>} goToMenu='main'></DropDownItem>
-            <DropDownItem leftIcon={<KeyboardArrowLeftIcon/>} >KUCOIN</DropDownItem>
-            <DropDownItem leftIcon={<KeyboardArrowLeftIcon/>} >GATE</DropDownItem>
-            <DropDownItem leftIcon={<KeyboardArrowLeftIcon/>} >PANCAKESWAP</DropDownItem>
-            <DropDownItem leftIcon={<KeyboardArrowLeftIcon/>} >UNISWAP</DropDownItem>
-            <DropDownItem leftIcon={<KeyboardArrowLeftIcon/>} >CRYPTOLOCALLY</DropDownItem>
+            <DropDownItem middleIcon={<KeyboardArrowRightIcon/>} goToMenu='main'></DropDownItem>
+            <DropDownItem leftIcon={<KeyboardArrowLeftIcon/>} ><a href="https://trade.kucoin.com/TLOS-USDT" target="_blank" rel="noreferrer"><Image src={Kucoin} alt="" style={{paddingLeft: '60px'}}/></a></DropDownItem>
+            <DropDownItem leftIcon={<KeyboardArrowLeftIcon/>} ><a href="https://www.gate.io/es/trade/TLOS_USDT" target="_blank" rel="noreferrer"><Image src={Gate} alt="" style={{transform: 'scale(1.2)', paddingLeft: '50px'}}/></a></DropDownItem>
+            <DropDownItem leftIcon={<KeyboardArrowLeftIcon/>} ><a href="https://pancakeswap.finance/swap?outputCurrency=0xb6c53431608e626ac81a9776ac3e999c5556717c" target="_blank" rel="noreferrer"><Image src={Pancake} alt="" style={{transform: 'scale(1.5)', paddingLeft: '50px'}}/></a></DropDownItem>
+            <DropDownItem leftIcon={<KeyboardArrowLeftIcon/>} ><a href="https://app.uniswap.org/#/swap?use=V2%3FinputCurrency%3DETH&outputCurrency=0x7825e833d495f3d1c28872415a4aee339d26ac88&chain=mainnet" target="_blank" rel="noreferrer"><Image src={Uniswap} alt="" style={{transform: 'scale(1.5)', paddingLeft: '50px'}}/></a></DropDownItem>
+            <DropDownItem leftIcon={<KeyboardArrowLeftIcon/>} ><a href="https://cryptolocally.com/en/crypto-offers/?type=buy&crypto=BTC&location=any&currency=any" target="_blank" rel="noreferrer"><Image src={Locally} alt="" style={{transform: 'scale(1.5)',paddingLeft: '50px', paddingTop: '5px'}}/></a></DropDownItem>
             </Menu>
+            </CSSTransition>
         </DropDown>
-        </CSSTransition>
+
+        <DropDown>
+        <CSSTransition in={activeMenu === 'build'} unmountOnExit timeout={500} classNames="menu-secondary">
+            <Menu>
+            <DropDownItem middleIcon={<KeyboardArrowRightIcon/>} goToMenu='main'></DropDownItem>
+            <DropDownItem leftIcon={<KeyboardArrowLeftIcon/>} rightIcon={<RoadMapIcon />} >ROADMAP</DropDownItem>
+            <DropDownItem leftIcon={<KeyboardArrowLeftIcon/>} rightIcon={<DevIcon/>} >DEVELOPERS</DropDownItem>
+            <DropDownItem leftIcon={<KeyboardArrowLeftIcon/>} rightIcon={<DocsIcon/>}>DOCUMENTATION</DropDownItem>
+            <DropDownItem leftIcon={<KeyboardArrowLeftIcon/>} rightIcon={<GrantIcon/>}>GRANT PROGRAM</DropDownItem>
+            </Menu>
+            </CSSTransition>
+        </DropDown>
+
+        <DropDown>
+        <CSSTransition in={activeMenu === 'learn'} unmountOnExit timeout={500} classNames="menu-secondary">
+            <Menu>
+            <DropDownItem middleIcon={<KeyboardArrowRightIcon/>} goToMenu='main'></DropDownItem>
+            <DropDownItem leftIcon={<KeyboardArrowLeftIcon/>} rightIcon={<CommIcon />} >COMMUNITY RESOURCES</DropDownItem>
+            <DropDownItem leftIcon={<KeyboardArrowLeftIcon/>} rightIcon={<BaseIcon/>} >KNOWLEDGE BASE</DropDownItem>
+            <DropDownItem leftIcon={<KeyboardArrowLeftIcon/>} rightIcon={<GovIcon/>}>GOVERNANCE</DropDownItem>
+            <DropDownItem leftIcon={<KeyboardArrowLeftIcon/>} rightIcon={<TokenIcon/>}>TLOS TOKENOMICS</DropDownItem>
+            <DropDownItem leftIcon={<KeyboardArrowLeftIcon/>} rightIcon={<Image src={ESG} alt="" style={{height: '23px', width: '23px', paddingRight: '3px'}}/>}>ESG BLOCKCHAIN</DropDownItem>
+            <DropDownItem leftIcon={<KeyboardArrowLeftIcon/>} rightIcon={<Image src={EVM} alt="" style={{height: '23px', width: '23px', paddingRight: '4px'}}/>}>EVM BLOCKCHAIN</DropDownItem>
+            </Menu>
+            </CSSTransition>
+        </DropDown>
+
+        <DropDown>
+        <CSSTransition in={activeMenu === 'evm-native'} unmountOnExit timeout={500} classNames="menu-secondary">
+            <Menu>
+            <DropDownItem middleIcon={<KeyboardArrowRightIcon/>} goToMenu='main'></DropDownItem>
+            <DropDownItem leftIcon={<KeyboardArrowLeftIcon />} rightIcon={<Image src={EVM} alt="" style={{height: '23px', width: '23px', paddingRight: '4px'}}/>} goToMenu="evm">EVM PLATFORM</DropDownItem>
+            <DropDownItem leftIcon={<KeyboardArrowLeftIcon/>} rightIcon={<Image src={TelosRoundLogo} alt="" style={{height: '23px', width: '23px', paddingRight: '4px'}}/>} goToMenu="native">NATIVE PLATFORM</DropDownItem>
+            </Menu>
+            </CSSTransition>
+        </DropDown>
+
+        <DropDown>
+        <CSSTransition in={activeMenu === 'evm'} unmountOnExit timeout={500} classNames="menu-secondary">
+            <Menu>
+            <DropDownItem middleIcon={<KeyboardArrowRightIcon/>} goToMenu='evm-native'></DropDownItem>
+            <DropDownItem rightIcon={<EVMExplorer/>}>BLOCK EXPLORER</DropDownItem>
+            <DropDownItem rightIcon={<EVMBridge/>}>BRIDGE</DropDownItem>
+            <DropDownItem rightIcon={<GetStarted/>}>GET STARTED</DropDownItem>
+            </Menu>
+            </CSSTransition>
+        </DropDown>
+
+        <DropDown>
+        <CSSTransition in={activeMenu === 'native'} unmountOnExit timeout={500} classNames="menu-secondary">
+            <Menu>
+            <DropDownItem middleIcon={<KeyboardArrowRightIcon/>} goToMenu='evm-native'></DropDownItem>
+            <DropDownItem rightIcon={<EcoIcon/>}>ECOSYSTEM</DropDownItem>
+            <DropDownItem rightIcon={<Wallet/>}>WEB WALLET</DropDownItem>
+            <DropDownItem rightIcon={<EVMExplorer/>}>BLOCK EXPLORER</DropDownItem>
+            <DropDownItem rightIcon={<EVMBridge/>}>BRIDGE</DropDownItem>
+            <DropDownItem rightIcon={<GetStarted/>}>GET STARTED</DropDownItem>
+            </Menu>
+            </CSSTransition>
+        </DropDown>
+
+        <DropDown>
+        <CSSTransition in={activeMenu === 'about'} unmountOnExit timeout={500} classNames="menu-secondary">
+            <Menu>
+            <DropDownItem middleIcon={<KeyboardArrowRightIcon/>} goToMenu='main'></DropDownItem>
+            <DropDownItem rightIcon={<NewsIcon/>}>NEWS</DropDownItem>
+            <DropDownItem rightIcon={<AboutIcon/>}>ABOUT</DropDownItem>
+            <DropDownItem rightIcon={<JoinIcon/>}>JOIN THE TEAM</DropDownItem>
+            <DropDownItem rightIcon={<MeetTeamIcon/>}>MEET THE TEAM</DropDownItem>
+            <DropDownItem rightIcon={<MeetBoardIcon/>}>MEET THE BOARD</DropDownItem>
+            </Menu>
+            </CSSTransition>
+        </DropDown>
+        
         </>
 
           

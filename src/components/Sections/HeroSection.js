94f7c9262f611ae93908ec8479efed0184b3408c
victor <bodegaflamenca666@gmail.com>
2022-02-27 14:28:43 +0100

    test

--- src/components/Sections/HeroSection.js
+++ src/components/Sections/HeroSection.js
@@ -123,10 +123,10 @@ function AnimationCanvas(){
 const Anim = styled.div`
     width: 100%;
     height: 100vh;
-    background-image: url(${LetterLogo});
+    /* background-image: url(${LetterLogo});
     background-repeat: no-repeat;
     background-size: 300px 100px;
-    background-position: 50% 40%;
+    background-position: 50% 40%; */
     background-color: ${props => props.theme.body};
     @media screen and (max-width: 768px){
         height: 100vh;
